9a85c5b1f902093bcc1909935edc32ded68b5f09
Tanishq Saini <tanishqsaini872@gmail.com>
2026-04-21 11:27:45 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://mern-auth-app-6pvc.onrender.com/api",
 });
 
 // Attach token to every request if available
